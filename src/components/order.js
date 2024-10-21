1df803982f084873d2752afd0d5b6057a8ecb8e1
antonbetak <190071@iberopuebla.mx>
2024-10-21 17:37:04 -0600

    estructura

---
+++ src/components/order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Order() {
+  return (
+    <div className="order-section">
+    </div>
+  );
+}
+
+export default Order;
---
+++ src/components/payment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Payment() {
+  return (
+    <div className="payment-section">
+     
+    </div>
+  );
+}
+
+export default Payment;
